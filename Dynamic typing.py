f0a5b4adf59f81903f86f2d7fc94f613488a040b
Glazkov-Kostya <JIaIIaJIyT@gmail.com>
2024-11-20 00:28:59 +0300

    Add files via upload

---
+++ Dynamic typing.py
@@ -0,0 +1,12 @@
+# переменная или идентификатор name/age/new_age
+name = 'Urban'
+print(name, type(name))
+name = 5
+print(name, type(name))
+name = 5.5
+print(name, type(name))
+name = [1,2]
+print(name, type(name))
+age = 30
+new_age = '30'
+# print(age+new_age) ошибка из-за разных типов данных
